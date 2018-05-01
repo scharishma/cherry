73d7239226309c5cca69203cb0d850b9bd216911
sai <cherry@ubuntu.ubuntu-domain>
2018-05-01 16:55:05 -0400

    commit

--- cherry/recipes/default.rb
+++ cherry/recipes/default.rb
@@ -3,3 +3,11 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+
+
+directory '/opt/cherry' do
+    owner 'root'
+    group 'root'
+    mode '0755'
+    action :create
+  end
